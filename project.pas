95b0733d74bdcf9050fd8c37bae18dc74ce4eb88
reaperjudge <38620064+reaperjudge@users.noreply.github.com>
2019-11-28 16:23:24 -0600

    added a lot to if statments

--- project.pas
+++ project.pas
@@ -1,7 +1,8 @@
 {$mode objfpc} // directive to be used for defining classes
-{$m+}		   // directive to be used for using constructor
-
+{$m+}	       // directive to be used for using constructor
 program exClass;
+Uses sysutils;
+
 type
    userinput = string;
    Sign = class
@@ -23,6 +24,7 @@ var
    m1: Sign;
    month : userinput;
    day : userinput;
+   dayint : integer;
 
 constructor Sign.create(l, w: integer);
 begin
@@ -63,6 +65,8 @@ begin
 end;
 
 begin
+
+
    Randomize; { This way we generate a new sequence every time
                the program is run}
 
@@ -73,60 +77,66 @@ begin
 
    writeln('Please enter you day of birth: ');
    readln(day);
-   if month = 'january' then
+   dayint := StrToInt(day);
+
+   if ((month = 'january') AND (dayint >= 20)) OR ((month = 'february') AND (dayint <= 18)) then
    begin
-        writeln('starting child');
+        writeln('Aquarius is the most humanitarian astrological sign.');
    end;
 
-   if month = 'february' then
+   if ((month = 'february') AND (dayint >= 19)) OR ((month = 'march') AND (dayint <= 20)) then
    begin
-        writeln('cold child');
-   end;
-   if month = 'march' then
-   begin
-        writeln('band child');
-   end;
-   if month = 'april' then
-   begin
-        writeln('rain child');
+        writeln('Pisces has absorbed every lesson - the joys and the pain, the hopes and the fears.');
    end;
 
-   if month = 'may' then
+   if ((month = 'march') AND (dayint >= 21)) OR ((month = 'april') AND (dayint <= 19)) then
    begin
-        writeln('mayday child');
+        writeln('Bold and ambitious, Aries dives headfirst into even the most challenging situations.');
    end;
 
-   if month = 'june' then
+   if ((month = 'april') AND (dayint >= 20)) OR ((month = 'may') AND (dayint <= 20)) then
    begin
-        writeln('summer child');
-   end;
-   if month = 'july' then
-   begin
-        writeln('independent child');
+        writeln('Like their celestial spirit animal, Taureans enjoy relaxing in serene,'+
+        'bucolic environments surrounded by soft sounds, soothing aromas, and succulent flavors.');
    end;
 
-   if month = 'august' then
+   if ((month = 'may') AND (dayint >= 21)) OR ((month = 'june') AND (dayint <= 20)) then
    begin
-        writeln('school child');
+        writeln('Appropriately symbolized by the celestial twins, this air sign was interested in so many pursuits that it had to double itself.');
    end;
 
-   if month = 'september' then
+   if ((month = 'june') AND (dayint >= 21)) OR ((month = 'july') AND (dayint <= 22)) then
    begin
-        writeln('septer child');
+        writeln('Cancers are highly intuitive and their psychic abilities manifest in tangible spaces');
    end;
 
-   if month = 'october' then
+   if ((month = 'july') AND (dayint >= 23)) OR ((month = 'august') AND (dayint <= 22)) then
    begin
-        writeln('halloween child');
+        writeln('Vivacious, theatrical, and passionate, Leos love to bask in the spotlight and celebrate themselves.');
    end;
 
-   if month = 'november' then
+   if ((month = 'august') AND (dayint >= 23)) OR ((month = 'september') AND (dayint <= 22)) then
    begin
-        writeln('novice child');
+        writeln('Virgos are logical, practical, and systematic in their approach to life');
    end;
 
-   if month = 'december' then
+   if ((month = 'september') AND (dayint >= 23)) OR ((month = 'october') AND (dayint <= 22)) then
    begin
-        writeln('snow child');
+        writeln(' Libra is obsessed with symmetry and strives to create equilibrium in all areas of life.');
+   end;
+
+   if ((month = 'october') AND (dayint >= 23)) OR ((month = 'november') AND (dayint <= 21)) then
+   begin
+        writeln('Scorpio is a water sign that derives its strength from the psychic, emotional realm.');
+   end;
+
+   if ((month = 'november') AND (dayint >= 22)) OR ((month = 'december') AND (dayint <= 21)) then
+   begin
+        writeln('sagittarius launches its many pursuits like blazing arrows, chasing after geographical, intellectual, and spiritual adventures.');
+   end;
+
+   if ((month = 'december') AND (dayint >= 22)) OR ((month = 'january') AND (dayint <= 19)) then
+   begin
+        writeln('Capricorns are skilled at navigating both the material and emotional realms.');
    end;
 end.
